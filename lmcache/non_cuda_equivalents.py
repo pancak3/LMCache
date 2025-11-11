e6390106ad770f1f27e8ca03962a70d77b6c58e9
Qifan Deng <qifan.deng@outlook.com>
2025-11-11 13:08:38 +1100

    add logs

--- lmcache/non_cuda_equivalents.py
+++ lmcache/non_cuda_equivalents.py
@@ -5,28 +5,29 @@
 #
 # Third Party
 import torch
-
+import logging
 # Store the tensor objects in memory so that they can be accessed
 # outside the scope of this file
 _tensor_registry: dict[int, torch.Tensor] = {}
 
+logger = logging.getLogger(__name__)
 
 def alloc_pinned_numa_ptr(size: int, numa_id: int = 0) -> int:
     """Non-CUDA equivalent of allocating pinned memory with NUMA awareness.
     Note: NUMA and pinned memory are not supported on non-CUDA."""
 
     # Create a 1D uint8 CPU tensor, as uint8 == 1 byte
-    print(f"[*] Warning: NUMA-aware pinned memory allocation is not supported on non-CUDA. Proceeding with standard pinned memory allocation.")
+    logger.info(f"[*] Warning: NUMA-aware pinned memory allocation is not supported on non-CUDA. Proceeding with standard pinned memory allocation.")
     tensor = torch.empty(size, dtype=torch.uint8, pin_memory=False)
-    print(f"[*] Allocating NUMA-aware pinned memory of size {size} bytes on NUMA node {numa_id}")
+    logger.info(f"[*] Allocating NUMA-aware pinned memory of size {size} bytes on NUMA node {numa_id}")
 
     # First-touch initialization (forces physical allocation)
     tensor.fill_(0)
-    print(f"[*] First-touch initialization completed")  
+    logger.info(f"[*] First-touch initialization completed")  
     # Get a pointer to the start of the tensor object as this is what is
     # returned by the CUDA equivalent function
     ptr = tensor.data_ptr()
-    print(f"[*] Obtained data pointer: {ptr}")
+    logger.info(f"[*] Obtained data pointer: {ptr}")
     # Store the tensor so it can be accessed outide this function scope
     _tensor_registry[ptr] = tensor
 
