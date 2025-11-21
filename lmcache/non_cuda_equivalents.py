9469ba2a4d96ae330ca7989b5696cc786faf65c6
Qifan Deng <qifan.deng@outlook.com>
2025-11-21 21:32:24 +1100

    try to fix mem leak

--- lmcache/non_cuda_equivalents.py
+++ lmcache/non_cuda_equivalents.py
@@ -4,30 +4,27 @@
 # CUDA-specific operations.
 #
 # Third Party
-import weakref
 import torch
-import logging
+
 # Store the tensor objects in memory so that they can be accessed
 # outside the scope of this file
-_tensor_registry: weakref.WeakValueDictionary[int, torch.Tensor] = weakref.WeakValueDictionary()
-logger = logging.getLogger(__name__)
+_tensor_registry: dict[int, torch.Tensor] = {}
+
 
 def alloc_pinned_numa_ptr(size: int, numa_id: int = 0) -> int:
     """Non-CUDA equivalent of allocating pinned memory with NUMA awareness.
     Note: NUMA and pinned memory are not supported on non-CUDA."""
 
     # Create a 1D uint8 CPU tensor, as uint8 == 1 byte
-    logger.info(f"[*] Warning: NUMA-aware pinned memory allocation is not supported on non-CUDA. Proceeding with standard pinned memory allocation.")
     tensor = torch.empty(size, dtype=torch.uint8, pin_memory=False)
-    logger.info(f"[*] Allocating NUMA-aware pinned memory of size {size} bytes on NUMA node {numa_id}")
 
     # First-touch initialization (forces physical allocation)
     tensor.fill_(0)
-    logger.info(f"[*] First-touch initialization completed")  
+
     # Get a pointer to the start of the tensor object as this is what is
     # returned by the CUDA equivalent function
     ptr = tensor.data_ptr()
-    logger.info(f"[*] Obtained data pointer: {ptr}")
+
     # Store the tensor so it can be accessed outide this function scope
     _tensor_registry[ptr] = tensor
 
@@ -46,16 +43,15 @@ def alloc_pinned_ptr(size: int, device_id: int = 0) -> int:
     to it. Note: Pinned memory is not supported on non-CUDA."""
 
     # Create a 1D uint8 CPU tensor, as uint8 == 1 byte
-    logger.info(f"[*] emptying pinned memory allocation of size {size} bytes")
     tensor = torch.empty(size, dtype=torch.uint8, pin_memory=False)
-    logger.info(f"[*] Allocating pinned memory of size {size} bytes")
+
     # First-touch initialization (forces physical allocation)
-    tensor.fill_(0)
-    logger.info(f"[*] First-touch initialization completed")
+    # tensor.fill_(0)
+
     # Get a pointer to the start of the tensor object as this is what is
     # returned by the CUDA equivalent function
     ptr = tensor.data_ptr()
-    logger.info(f"[*] Obtained data pointer: {ptr}")
+
     # Store the tensor so it can be accessed outide this function scope
     _tensor_registry[ptr] = tensor
 
